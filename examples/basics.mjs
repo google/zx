c242f07c6972061e0f46a5c65ae103c4bf0dc3e1
Salah Othman <salah_elhossiny856@yahoo.com>
2021-05-11 09:05:05 +0200

    Update basics.mjs

--- examples/basics.mjs
+++ examples/basics.mjs
@@ -14,6 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
 await $`ls -1 | wc -l`
 
 let branch = await $`git branch --show-current`
