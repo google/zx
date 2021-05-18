6883dba8858a9b9a6c00c18dfc8f3e5278401c2d
Anton Medvedev <anton@medv.io>
2021-05-19 01:00:02 +0200

    Fix order

--- index.d.ts
+++ index.d.ts
@@ -25,11 +25,10 @@ export const $: $
 
 export function cd(path: string)
 
-type QuestionOptions = { choices: string[] }
+export function question(query?: string, options?: QuestionOptions): Promise<string>
+export type QuestionOptions = { choices: string[] }
 
 export function sleep(ms: number): Promise<void>
-
-export function question(query?: string, options?: QuestionOptions): Promise<string>
 
 export class ProcessOutput {
   readonly exitCode: number
