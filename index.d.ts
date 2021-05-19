4a62921e93a420b18462fb78174813853718d89a
Khải <hvksmr1996@gmail.com>
2021-05-19 10:27:34 +0700

    Why does `cd` lacks return type?

--- index.d.ts
+++ index.d.ts
@@ -23,7 +23,7 @@ interface $ {
 
 export const $: $
 
-export function cd(path: string)
+export function cd(path: string): Promise<void>
 
 export function question(query?: string, options?: QuestionOptions): Promise<string>
 export type QuestionOptions = { choices: string[] }
