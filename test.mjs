b82f3407fe686f4fe866f3a9bb4a59d216808477
Anton Medvedev <anton@medv.io>
2021-05-22 07:30:58 +0200

    Debug

--- test.mjs
+++ test.mjs
@@ -88,10 +88,10 @@ import {strict as assert} from 'assert'
 }
 
 { // Pipes are working
-  // let {stdout} = await $`echo "hello"`
-  //   .pipe($`awk '{print $1" world"}'`)
-  //   .pipe($`tr '[a-z]' '[A-Z]'`)
-  // assert(stdout === 'HELLO WORLD\n')
+  let {stdout} = await $`echo "hello"`
+    .pipe($`awk '{print $1" world"}'`)
+    .pipe($`tr '[a-z]' '[A-Z]'`)
+  assert(stdout === 'HELLO WORLD\n')
 
   // try {
   //   let w = await $`echo foo`
