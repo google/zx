2411c3796fd9cce1cc17fba8217049623bbf8b31
Anton Medvedev <anton@medv.io>
2021-05-25 21:16:38 +0200

    Update examples

--- examples/basics.mjs
+++ examples/basics.mjs
@@ -16,13 +16,33 @@
 
 await $`ls -1 | wc -l`
 
+await Promise.all([
+  $`sleep 1; echo 1`,
+  $`sleep 2; echo 2`,
+  $`sleep 3; echo 3`,
+])
+
 let branch = await $`git branch --show-current`
 await $`printf ${branch} | wc` // The new line trimmed from stdout.
 
 let foo = `hi; echo 'oops'`
 await $`echo ${foo} | wc` // Vars properly quoted.
 
-// We can use import and require together.
-let path = await import('path')
-let {name} = require(path.join(__dirname, '..', 'package.json'))
-console.log(chalk.black.bgCyanBright(name))
+let path = await import('path') // We can use import,
+let {name} = require(path.join(__dirname, '..', 'package.json')) // and require.
+
+console.log(chalk.black.bgCyanBright(name)) // The chalk is available without import.
+
+try {
+  await $`exit 42` // ProcessOutput will be thrown on non-zero exit codes.
+} catch (p) {
+  console.log(p.exitCode)
+}
+
+let {exitCode} = await nothrow($`exit 42`) // Change behavior to no-throw.
+exitCode = await $`exit 42`.exitCode // Or wait on exitCode itself.
+
+// Use combinations of pipe() and nothrow():
+await $`find ./examples -type f -print0`
+  .pipe(nothrow($`xargs -0 grep ${'missing' + 'part'}`))
+  .pipe($`wc -l`)
--- examples/markdown.md
+++ examples/markdown.md
@@ -1,14 +1,14 @@
 # Markdown Scripts
 
 It's possible to write scripts using markdown. Only code blocks will be executed
-by zx. Try to run `zx examples/index.md`.
+by zx. Try to run `zx examples/markdown.md`.
 
 ```js
 await $`whoami`
 await $`ls -la ${__dirname}`
 ```
 
-The `__filename` will be pointed to **index.md**:
+The `__filename` will be pointed to **markdown.md**:
 
 ```js
 console.log(chalk.yellowBright(__filename))
