ff92781637186abd3a629f4614b48e1895e178ed
Rahul <optimbro@outlook.com>
2022-04-09 22:07:35 +0530

    add types for 'prompts' package

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,11 @@
 // Copyright 2021 Google LLC
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     https://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,7 @@ import {Readable, Writable} from 'stream'
 import * as _fs from 'fs-extra'
 import * as _globby from 'globby'
 import * as _os from 'os'
+import * as _prompts from 'prompts';
 import * as _path from 'path'
 import {ChalkInstance} from 'chalk'
 import * as _yaml from 'yaml'
@@ -77,6 +78,7 @@ export const glob: typeof _globby.globby & typeof _globby
 export const globby: typeof _globby.globby & typeof _globby
 export const nothrow: nothrow
 export const os: typeof _os
+export const prompts: typeof _prompts;
 export const path: typeof _path
 export const question: question
 export const sleep: sleep
