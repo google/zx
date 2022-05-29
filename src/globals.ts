c296c6ad35fa3d50d136251613ad293c3401570e
Anton Medvedev <anton@medv.io>
2022-05-30 01:11:02 +0200

    Update deps

--- src/globals.ts
+++ src/globals.ts
@@ -7,9 +7,6 @@ declare global {
   var argv: typeof _.argv
   var cd: typeof _.cd
   var chalk: typeof _.chalk
-  // @ts-ignore
-  // TODO: This not working for some reason.
-  // var fetch: typeof _.fetch
   var fs: typeof _.fs
   var glob: typeof _.glob
   var globby: typeof _.globby
