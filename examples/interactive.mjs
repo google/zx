2fc0aa5a578bb984700bc21ebf3aa4a556e7a396
Anton Medvedev <anton@medv.io>
2022-05-31 23:28:52 +0200

    Update interactive.mjs example

--- examples/interactive.mjs
+++ examples/interactive.mjs
@@ -16,20 +16,15 @@
 
 let { stdin, stdout } = $`npm init`
 
-let put = (text) => {
-  stdin.write(text)
-  process.stdout.write(text)
-}
-
 for await (let chunk of stdout) {
-  if (chunk.includes('package name:')) put('test\n')
-  if (chunk.includes('version:')) put('1.0.0\n')
-  if (chunk.includes('description:')) put('My test package\n')
-  if (chunk.includes('entry point:')) put('index.mjs\n')
-  if (chunk.includes('test command:')) put('test.mjs\n')
-  if (chunk.includes('git repository:')) put('my-org/repo\n')
-  if (chunk.includes('keywords:')) put('foo, bar\n')
-  if (chunk.includes('author:')) put('Anton Medvedev\n')
-  if (chunk.includes('license:')) put('MIT\n')
-  if (chunk.includes('Is this OK?')) put('yes\n')
+  if (chunk.includes('package name:')) stdin.write('test\n')
+  if (chunk.includes('version:')) stdin.write('1.0.0\n')
+  if (chunk.includes('description:')) stdin.write('My test package\n')
+  if (chunk.includes('entry point:')) stdin.write('index.mjs\n')
+  if (chunk.includes('test command:')) stdin.write('test.mjs\n')
+  if (chunk.includes('git repository:')) stdin.write('my-org/repo\n')
+  if (chunk.includes('keywords:')) stdin.write('foo, bar\n')
+  if (chunk.includes('author:')) stdin.write('Anton Medvedev\n')
+  if (chunk.includes('license:')) stdin.write('MIT\n')
+  if (chunk.includes('Is this OK?')) stdin.write('yes\n')
 }
