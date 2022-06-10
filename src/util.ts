1104a2d18f3b037e792c6d8aa72afa767f21337e
Anton Golub <antongolub@antongolub.com>
2022-06-10 15:01:18 +0300

    feat: add NaN assert to duration parser

--- src/util.ts
+++ src/util.ts
@@ -112,14 +112,17 @@ export type Duration = number | `${number}s` | `${number}ms`
 
 export function parseDuration(d: Duration) {
   if (typeof d == 'number') {
+    if (isNaN(d) || d < 0) {
+      throw new Error(`Invalid duration: "${d}".`)
+    }
     return d
   } else if (/\d+s/.test(d)) {
     return +d.slice(0, -1) * 1000
   } else if (/\d+ms/.test(d)) {
     return +d.slice(0, -2)
-  } else {
-    throw new Error(`Unknown duration: "${d}".`)
   }
+
+  throw new Error(`Unknown duration: "${d}".`)
 }
 
 export function formatCmd(cmd?: string): string {
