88ffdc51bf3277bcbb9c29d4d2b60f528dfc8716
Ole Bergen <54313166+olebergen@users.noreply.github.com>
2022-06-13 14:31:08 +0200

    Make options optional

--- src/question.ts
+++ src/question.ts
@@ -14,7 +14,7 @@
 
 import { createInterface } from 'node:readline'
 
-export async function question(query: string, options: { choices: string[] }) {
+export async function question(query: string, options?: { choices: string[] }) {
   let completer = undefined
   if (Array.isArray(options?.choices)) {
     completer = function completer(line: string) {
