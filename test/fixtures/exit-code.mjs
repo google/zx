91ea0926dd19e1d14af3eda461e8a05c47484a3d
Ibrahim Memon <106296670+ibrahimmemonn@users.noreply.github.com>
2022-10-20 17:43:46 +0500

    Update exit-code.mjs

--- test/fixtures/exit-code.mjs
+++ test/fixtures/exit-code.mjs
@@ -4,6 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
+// LINK BELOW
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
