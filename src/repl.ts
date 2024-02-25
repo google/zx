65185fe64b9a39dd8256fa71efb750d4e2a99bd3
Anton Medvedev <anton@medv.io>
2024-02-25 14:40:04 +0100

    Dev (#724)
    * Only import repl if needed
    
    * await node:repl
    
    * Do not throw on win which

--- src/repl.ts
+++ src/repl.ts
@@ -14,14 +14,13 @@
 
 import os from 'node:os'
 import path from 'node:path'
-import repl from 'node:repl'
 import { inspect } from 'node:util'
 import { ProcessOutput, defaults } from './core.js'
 import { chalk } from './vendor.js'
 
-export function startRepl() {
+export async function startRepl() {
   defaults.verbose = false
-  const r = repl.start({
+  const r = (await import('node:repl')).start({
     prompt: chalk.greenBright.bold('❯ '),
     useGlobal: true,
     preview: false,
