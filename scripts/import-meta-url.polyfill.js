3c680206f6dfd85398e806c8bbd09148c4ba3798
Anton Golub <antongolub@antongolub.com>
2024-09-12 22:36:31 +0300

    chore: tweak up import-meta-url snippet

--- scripts/import-meta-url.polyfill.js
+++ scripts/import-meta-url.polyfill.js
@@ -1,5 +1,5 @@
 const import_meta_url =
   typeof document === 'undefined'
-    ? new (require('url'.replace('', '')).URL)('file:' + __filename).href
+    ? new (require('url').URL)('file:' + __filename).href
     : (document.currentScript && document.currentScript.src) ||
       new URL('main.js', document.baseURI).href
