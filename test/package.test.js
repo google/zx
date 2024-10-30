75482ee3a72265740a38c01da195f7f2c8abbc36
Anton Golub <antongolub@antongolub.com>
2024-10-30 22:26:15 +0300

    test: refactor npm pack test (#926)
    * test: refactor npm pack test
    
    * test: refactor copyright test

--- test/package.test.js
+++ test/package.test.js
@@ -13,22 +13,77 @@
 // limitations under the License.
 
 import assert from 'node:assert'
-import { test, describe, beforeEach, before, after } from 'node:test'
+import { test, describe, after, before } from 'node:test'
 import '../build/globals.js'
 
+const __dirname = new URL('.', import.meta.url).pathname
+const root = path.resolve(__dirname, '..')
+
 describe('package', () => {
-  before(() => syncProcessCwd())
-  after(() => syncProcessCwd(false))
-  beforeEach(async () => {
+  before(async () => {
     const pack = await $`npm pack`
     await $`tar xf ${pack}`
     await $`rm ${pack}`.nothrow()
   })
+  after(async () => {
+    await $`rm -rf package`
+  })
+
+  test('content looks fine', async () => {
+    const files = await glob('**/*', {
+      cwd: path.resolve(root, 'package'),
+      absolute: false,
+      onlyFiles: true,
+    })
+    assert.deepEqual(
+      files.sort(),
+      [
+        'LICENSE',
+        'README.md',
+        'package.json',
+        'man/zx.1',
+        'build/cli.cjs',
+        'build/cli.d.ts',
+        'build/cli.js',
+        'build/core.cjs',
+        'build/core.d.ts',
+        'build/core.js',
+        'build/deno.js',
+        'build/deps.cjs',
+        'build/deps.d.ts',
+        'build/deps.js',
+        'build/esblib.cjs',
+        'build/globals.cjs',
+        'build/globals.d.ts',
+        'build/globals.js',
+        'build/goods.cjs',
+        'build/goods.d.ts',
+        'build/goods.js',
+        'build/index.cjs',
+        'build/index.d.ts',
+        'build/index.js',
+        'build/repl.cjs',
+        'build/repl.d.ts',
+        'build/repl.js',
+        'build/util.cjs',
+        'build/util.d.ts',
+        'build/util.js',
+        'build/vendor-core.cjs',
+        'build/vendor-core.d.ts',
+        'build/vendor-core.js',
+        'build/vendor-extra.cjs',
+        'build/vendor-extra.d.ts',
+        'build/vendor-extra.js',
+        'build/vendor.cjs',
+        'build/vendor.d.ts',
+        'build/vendor.js',
+      ].sort()
+    )
+  })
 
   test('ts project', async () => {
-    const pack = path.resolve('package')
     const out = await within(async () => {
-      cd('test/fixtures/ts-project')
+      $.cwd = path.resolve(__dirname, 'fixtures/ts-project')
       await $`npm i --no-package-lock`
       try {
         await $`npx tsc`
@@ -41,9 +96,8 @@ describe('package', () => {
   })
 
   test('js project with zx', async () => {
-    const pack = path.resolve('package')
     const out = await within(async () => {
-      cd('test/fixtures/js-project')
+      $.cwd = path.resolve(__dirname, 'fixtures/js-project')
       await $`npm i --no-package-lock`
       return $`node node_modules/zx/build/cli.js --verbose script.js`
     })
