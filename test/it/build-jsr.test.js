ab2edcf26421078d0a0fbeffcb0380e0477c83c0
Anton Golub <antongolub@antongolub.com>
2024-12-15 22:03:19 +0300

    test: optimize jsr artifact test

--- test/it/build-jsr.test.js
+++ test/it/build-jsr.test.js
@@ -17,39 +17,36 @@ import { describe, before, after, it } from 'node:test'
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 const root = path.resolve(__dirname, '../../')
-const pkgJson = JSON.parse(
-  fs.readFileSync(path.resolve(root, 'package.json'), 'utf-8')
-)
 
-describe('jsr artefact', () => {
+describe('jsr artifact', () => {
   let tmp
   let t$
 
   before(async () => {
     tmp = tempdir()
     t$ = $({ cwd: tmp, quiet: true })
-    path.resolve(tmp, 'node_modules/zx')
-    await fs.outputJSON(path.resolve(tmp, 'package.json'), pkgJson)
-
-    await t$`npm i`
 
     // copy all for jsr publish
     await Promise.all(
-      ['src/', 'tsconfig.json', 'LICENSE', 'scripts/build-jsr.mjs'].map(
-        async (filepath) => {
-          return await fs.copy(
-            path.resolve(path.join(root, filepath)),
-            path.resolve(path.join(tmp, filepath))
-          )
-        }
+      [
+        'src/',
+        'tsconfig.json',
+        'LICENSE',
+        'scripts/build-jsr.mjs',
+        'package.json',
+      ].map((filepath) =>
+        fs.copy(
+          path.resolve(path.join(root, filepath)),
+          path.resolve(path.join(tmp, filepath))
+        )
       )
     )
+    await t$`ln -s ${path.resolve(root, 'node_modules')} ${path.resolve(tmp, 'node_modules')}`
   })
-
   after(() => fs.remove(tmp))
 
   it('publish --dry-run --allow-dirty`', async () => {
     await t$`node scripts/build-jsr.mjs`
-    await t$`npx jsr publish --dry-run --allow-dirty`
+    await t$({ quiet: false })`jsr publish --dry-run --allow-dirty`
   })
 })
