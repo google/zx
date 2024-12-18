123c28641db397e16cc5cadba1c9aaa7a7d53474
Mikhail Avdeev <easy-mikey@ya.ru>
2024-12-18 13:51:33 +0300

    feat: create script for omit field

---
+++ test/it/clean-package-json.test.js
@@ -0,0 +1,72 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import assert from 'node:assert'
+import { tempdir, $, path, fs } from '../../build/index.js'
+import { describe, before, after, it } from 'node:test'
+import { createRequire } from 'node:module'
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const root = path.resolve(__dirname, '../../')
+const whitelist = [
+  'name',
+  'version',
+  'description',
+  'type',
+  'main',
+  'types',
+  'typesVersions',
+  'exports',
+  'bin',
+  'man',
+  'files',
+  'engines',
+  'optionalDependencies',
+  'publishConfig',
+  'keywords',
+  'repository',
+  'homepage',
+  'author',
+  'license',
+]
+
+describe('package.json artifact', () => {
+  let tmp
+  let t$
+
+  before(async () => {
+    tmp = tempdir()
+    t$ = $({ cwd: tmp, quiet: true })
+
+    await fs.copy(
+      path.resolve(root, 'package.json'),
+      path.resolve(tmp, 'package.json')
+    )
+    await fs.copy(
+      path.resolve(root, 'scripts/clean-package-json.mjs'),
+      path.resolve(tmp, 'scripts/clean-package-json.mjs')
+    )
+  })
+
+  after(() => fs.remove(tmp))
+
+  it('handle exist properties required for publishing', async () => {
+    await t$`node scripts/clean-package-json.mjs`
+    // to throw if manifest is not correct
+    const file = createRequire(import.meta.url)(
+      path.resolve(tmp, 'package.json')
+    )
+    assert.strictEqual(whitelist, Object.keys(file))
+  })
+})
