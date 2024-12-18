6a776b1e07976dab0a3e675e27b282d2aa265dd1
Anton Golub <antongolub@antongolub.com>
2024-12-18 20:01:46 +0300

    test: merge autogenerated vendor tests
    continues #987

--- test/vendor-yaml.test.js
+++ test/vendor-yaml.test.js
@@ -16,38 +16,12 @@ import assert from 'node:assert'
 import { test, describe } from 'node:test'
 import { YAML } from '../build/vendor.js'
 
-describe('vendor YAML API ', () => {
-  test('exports', () => {
-    assert.equal(typeof YAML, 'object')
-    assert.equal(typeof YAML.Alias, 'function')
-    assert.equal(typeof YAML.CST, 'object')
-    assert.equal(typeof YAML.Composer, 'function')
-    assert.equal(typeof YAML.Document, 'function')
-    assert.equal(typeof YAML.Lexer, 'function')
-    assert.equal(typeof YAML.LineCounter, 'function')
-    assert.equal(typeof YAML.Pair, 'function')
-    assert.equal(typeof YAML.Parser, 'function')
-    assert.equal(typeof YAML.Scalar, 'function')
-    assert.equal(typeof YAML.Schema, 'function')
-    assert.equal(typeof YAML.YAMLError, 'function')
-    assert.equal(typeof YAML.YAMLMap, 'function')
-    assert.equal(typeof YAML.YAMLParseError, 'function')
-    assert.equal(typeof YAML.YAMLSeq, 'function')
-    assert.equal(typeof YAML.YAMLWarning, 'function')
-    assert.equal(typeof YAML.default, 'object')
-    assert.equal(typeof YAML.isAlias, 'function')
-    assert.equal(typeof YAML.isCollection, 'function')
-    assert.equal(typeof YAML.isDocument, 'function')
-    assert.equal(typeof YAML.isMap, 'function')
-    assert.equal(typeof YAML.isNode, 'function')
-    assert.equal(typeof YAML.isPair, 'function')
-    assert.equal(typeof YAML.isScalar, 'function')
-    assert.equal(typeof YAML.isSeq, 'function')
-    assert.equal(typeof YAML.parse, 'function')
-    assert.equal(typeof YAML.parseAllDocuments, 'function')
-    assert.equal(typeof YAML.parseDocument, 'function')
-    assert.equal(typeof YAML.stringify, 'function')
-    assert.equal(typeof YAML.visit, 'function')
-    assert.equal(typeof YAML.visitAsync, 'function')
+describe('YAML', () => {
+  test('YAML.parse', () => {
+    assert.deepEqual(YAML.parse('a: b\n'), { a: 'b' })
+  })
+
+  test('YAML.stringify', () => {
+    assert.equal(YAML.stringify({ a: 'b' }), 'a: b\n')
   })
 })
