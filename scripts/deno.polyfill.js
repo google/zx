9e41de5a8ed96f40ed75e867df413412ae0ed9f0
Anton Golub <antongolub@antongolub.com>
2025-01-07 19:28:52 +0300

    chore(deno): tweak up process polyfill

--- scripts/deno.polyfill.js
+++ scripts/deno.polyfill.js
@@ -10,12 +10,12 @@ if (globalThis.Deno) {
   globalThis.__filename = __filename
   globalThis.__dirname = __dirname
   globalThis.module = new Proxy({}, { set() { return true } })
-  globalThis.process = {
+  Object.assign(globalThis.process, {
     version: 'v18.0.0',
     versions: { node: '18.0.0' },
     env: globalThis.Deno.env.toObject(),
     argv: [globalThis.Deno.execPath(), globalThis.Deno.mainModule.replace('file://', ''), ...globalThis.Deno.args]
-  }
+  })
 }
 
 export { require, __dirname, __filename }
