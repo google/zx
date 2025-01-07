022606a7bbd2d04a6f219e2e3f0d945824b353ec
Anton Golub <antongolub@antongolub.com>
2025-01-07 20:59:38 +0300

    chore(deno): minor polyfill refactoring (#1062)

--- scripts/deno.polyfill.js
+++ scripts/deno.polyfill.js
@@ -1,15 +1,11 @@
 import { createRequire } from 'node:module'
 import * as process from 'node:process'
 
-const require = createRequire(import.meta.url)
-const __filename = new URL(import.meta.url).pathname
-const __dirname = new URL('.', import.meta.url).pathname
-
 // prettier-ignore
 if (globalThis.Deno) {
-  globalThis.require = require
-  globalThis.__filename = __filename
-  globalThis.__dirname = __dirname
+  globalThis.require = createRequire(import.meta.url)
+  globalThis.__filename = new URL(import.meta.url).pathname
+  globalThis.__dirname = new URL('.', import.meta.url).pathname
   globalThis.module = new Proxy({}, { set() { return true } })
 
   const p = globalThis.process = globalThis.process || process
@@ -18,5 +14,3 @@ if (globalThis.Deno) {
   p.env || (p.env = globalThis.Deno.env.toObject())
   p.argv || (p.argv = [globalThis.Deno.execPath(), globalThis.Deno.mainModule.replace('file://', ''), ...globalThis.Deno.args])
 }
-
-export { require, __dirname, __filename }
