3db02fe28bf34c828662aa4e3c0e3aff58ab4038
Anton Golub <antongolub@antongolub.com>
2025-01-10 15:27:08 +0300

    test(bench): add buf-join aliternatives

--- src/core.ts
+++ src/core.ts
@@ -520,7 +520,10 @@ export class ProcessPromise extends Promise<ProcessOutput> {
     return this
   }
 
-  timeout(d: Duration, signal = this._timeoutSignal || $.timeoutSignal): ProcessPromise {
+  timeout(
+    d: Duration,
+    signal = this._timeoutSignal || $.timeoutSignal
+  ): ProcessPromise {
     if (this._resolved) return this
 
     this._timeout = parseDuration(d)
