4d1087f7230c5812f264a46ca46c56a780c1379f
Anton Golub <antongolub@antongolub.com>
2025-06-23 19:46:41 +0300

    ci: configure test and publish workflows

--- test/cli.test.js
+++ test/cli.test.js
@@ -123,8 +123,10 @@ test('scripts from https', async () => {
 
 test('scripts from https not ok', async () => {
   const port = await getPort()
-  const server = await getServer(['HTTP/1.1 500\n\n']).listen(port)
+  const resp = await fs.readFile(path.resolve('test/fixtures/500.http'))
+  const server = await getServer([resp]).listen(port)
   const out = await $`node build/cli.js http://127.0.0.1:${port}`.nothrow()
+  console.log(out.toString())
   assert.match(out.stderr, /Error: Can't get/)
   await server.stop()
 })
