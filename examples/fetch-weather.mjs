15f1845e3b6ea1986248c7ee006b37cbde57b883
Anton Golub <golub.anton@gmail.com>
2025-07-06 20:50:59 +0300

    chore: restore license formatting

--- examples/fetch-weather.mjs
+++ examples/fetch-weather.mjs
@@ -3,7 +3,7 @@
 // Copyright 2021 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// You may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     https://www.apache.org/licenses/LICENSE-2.0
