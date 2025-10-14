bdf577bdaec24c3fca9d2412dbe3e4c385174cc2
Anton Golub <antongolub@antongolub.com>
2025-10-14 11:10:07 +0300

    chore: update globby to v15

--- src/versions.ts
+++ src/versions.ts
@@ -19,7 +19,7 @@ export const versions: Record<string, string> = {
   dotenv: '0.2.3',
   fetch: '1.6.7',
   fs: '11.3.2',
-  glob: '14.1.0',
+  glob: '15.0.0',
   minimist: '1.2.8',
   ps: '1.0.0',
   which: '5.0.0',
